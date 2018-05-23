02e21a66d4067e9fdb0ecfe34daf6d871d49e141
zeng gang <gang.zeng@shopee.com>
2018-05-23 11:01:56 +0800

    updatr

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/smtp"
+	"os"
+	"reflect"
+	"runtime"
+	"time"
 
-	"git.garena.com/lixh/goorm"
+	"github.com/gogo/protobuf/proto"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	gmail "google.golang.org/api/gmail/v1"
 )
 
 func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
@@ -100,35 +111,176 @@ const token = "bSI1MtWOn3ccBJjf5Y2ZBE8ZZEQsQPqG"
 
 // }
 
-func log1(format string, arg ...interface{}) {
-	fmt.Sprintf(format, arg...)
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
 }
 
-func init() {
-	goorm.RegisterLogFunction(log1, true)
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
 
-	goorm.RegisterDataBase("entry_db", "mysql", 10, 10, true, "dev:dev@tcp(127.0.0.1:3306)/entry_db?timeout=10s")
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
 
-	// user := &User{}
+	tok, err := config.Exchange(oauth2.NoContext, authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
 
-	// mi := goorm.RegisterModel(user)
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
 
-	// mi.SetPrimayColumn("id", true)
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	defer f.Close()
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	json.NewEncoder(f).Encode(token)
+}
 
-	// mi.SetSplitInfo("userid", SplitKeyMethodMod, 10, "")
+func gmailtest() {
+	b, err := ioutil.ReadFile("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved client_secret.json.
+	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	srv, err := gmail.New(getClient(config))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
+
+	user := "me"
+	r, err := srv.Users.Labels.List(user).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve labels: %v", err)
+	}
+	if len(r.Labels) == 0 {
+		fmt.Println("No labels found.")
+		return
+	}
+	fmt.Println("Labels:")
+	for _, l := range r.Labels {
+		fmt.Printf("- %s\n", l.Name)
+	}
+}
+
+var i int32 = 0
+
+var queue = make(chan string, 2)
+
+func read() {
+	for elem := range queue {
+		fmt.Println(elem)
+	}
+	fmt.Println("done")
 }
 
 func main() {
+	a := make([]int, 0, 10)
+	a = append(a, 123)
+	fmt.Printf("a: %d,", a)
+	go read()
+	for i := 0; i < 20; i++ {
+		queue <- fmt.Sprintf("my: %d", i)
+		time.Sleep(time.Millisecond * 100)
 
-	const s = "48656c6c6f20476f7068657221"
-	decoded, err := hex.DecodeString(s)
-	if err != nil {
-		log.Fatal(err)
 	}
 
-	fmt.Printf("%s\n", decoded)
+	fmt.Println("#####")
+	close(queue)
 
-	s1 := hex.EncodeToString(decoded)
-	fmt.Printf("%s \n", s1)
+	time.Sleep(time.Second * 100)
+	fmt.Println("##### exit")
 
+}
+
+func testerror() (err error) {
+	defer func() {
+		fmt.Printf("BB: %v\n", err)
+
+	}()
+	fmt.Printf("AA: %v\n", err)
+	return
+}
+
+func getResult(a proto.Message) {
+	fmt.Printf("----- \n")
+	val := reflect.Indirect(reflect.ValueOf(a))
+	fmt.Println(val.Type().Field(0))
+	fmt.Printf("----- \n")
+
+	x := reflect.ValueOf(a).Elem().FieldByName("Result")
+	code := x.Elem().FieldByName("ErrorCode")
+	msg := x.Elem().FieldByName("ErrorMsg")
+
+	fmt.Printf("** %+v \n %v \n %v\n", x, code, msg)
+
+	a1, ok := code.Interface().(*int32)
+	fmt.Printf("## ** %v %v \n", *a1, ok)
+	a2, ok := msg.Interface().(*string)
+	fmt.Printf("## ** %v %v \n", *a2, ok)
+
+	// val.Type().Field(0).Name
+	// val = val.FieldByName("result")
+	// fmt.Printf("val: %+v \n", val.Name)
+
+	pc := make([]uintptr, 10) // at least 1 entry needed
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	file, line := f.FileLine(pc[0])
+	fmt.Printf("%s:%d %s\n", file, line, f.Name())
+
+}
+
+func testemail() {
+	from := "gang.zeng@shopee.com"
+	pass := "roiokmvgenobtpgh"
+	to := "gang.zeng@shopee.com"
+
+	body := "test 123 曾刚 from sz 12 2355555"
+	msg := "From: " + from + "\n" +
+		"To: " + to + "\n" +
+		"Subject: Hello there\n\n" +
+		body
+
+	err := smtp.SendMail("smtp.gmail.com:587",
+		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+		from, []string{to}, []byte(msg))
+
+	if err != nil {
+		log.Printf("smtp error: %s", err)
+		return
+	}
+	return
 }
